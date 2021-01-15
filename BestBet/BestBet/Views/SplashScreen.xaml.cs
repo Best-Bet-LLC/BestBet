5d5bf762eb5274fedf7804c05a8226b5cff28347
christopherdeck <cdeck20@optonline.net>
2021-01-14 20:55:44 -0500

    * SplashScreen.cs: * SplashScreen.xaml: * SplashScreen.xaml.cs: * splashscreen.json: * splashscreen.json: * splashscreen_gold.json: * SplashScreenAnimation.xml: * 301_search_location.json: * SplashLayout.xml: * wordmark_edited.png:
    * .DS_Store: splash screen stuff

---
+++ BestBet/BestBet/Views/SplashScreen.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BestBet.Views
+{
+    public partial class SplashScreen : ContentPage
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
